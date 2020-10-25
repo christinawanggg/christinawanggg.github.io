a7aab12a0f344fdc7063dcae2844dccc598e4277
christinawanggg <christinawang2000@gmail.com>
2020-10-25 00:16:31 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// taking away link extensions
+$("").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
+
+// Changes the background color of navbar once you scroll
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
